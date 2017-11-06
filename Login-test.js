f07551ed41aaf12be17b0b1a15da7f7d5f27d5bd
mahiravlajcic <33397801+mahiravlajcic@users.noreply.github.com>
2017-11-06 13:50:49 +0100

    login-test.js

--- Login-test.js
+++ Login-test.js
@@ -15,37 +15,31 @@ describe("Login Page", function () {
 
         expect(pageTitle.getText()).toBe("Welcome, Please Sign In!")
 
+    })
+
+    it('Verify that user can not login with invalid email input', function () {
+        var loginMenuLink = element(by.css('.ico-login'));
+        var emailInput = $('#Email')
+        var passwordInput = $('#Password')
+        var loginButton = $('.  login-button')
+        var wrongEmailMessage = $('.field-validation-error')
 
 
+        loginMenuLink.click();
+
+        emailInput.sendKeys("test@.com")
+        passwordInput.sendKeys("test123")
+        loginButton.click();
+
+        browser.sleep(5000)
+
+        expect(wrongEmailMessage.getText()).toContain('Wrong email');
 
     })
 
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-)
 
 
 
