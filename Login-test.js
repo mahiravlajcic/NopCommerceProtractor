94de57864670ccd5cf6cb13b351bf9d48eb41414
mahiravlajcic <33397801+mahiravlajcic@users.noreply.github.com>
2017-11-06 15:19:47 +0100

    login-test.js

--- Login-test.js
+++ Login-test.js
@@ -21,7 +21,7 @@ describe("Login Page", function () {
         var loginMenuLink = element(by.css('.ico-login'));
         var emailInput = $('#Email')
         var passwordInput = $('#Password')
-        var loginButton = $('.  login-button')
+        var loginButton = $('.login-button')
         var wrongEmailMessage = $('.field-validation-error')
 
 
@@ -34,6 +34,28 @@ describe("Login Page", function () {
         browser.sleep(5000)
 
         expect(wrongEmailMessage.getText()).toContain('Wrong email');
+
+    })
+
+
+    it('Verify that user can not login with invalid password input', function () {
+        var loginMenuLink = element(by.css('.ico-login'));
+        var emailInput = $('#Email')
+        var passwordInput = $('#Password')
+        var loginButton = $('.login-button')
+        var validationMessage = $('.validation-summary-errors')
+
+
+        loginMenuLink.click();
+
+        emailInput.sendKeys("test@gmail.com")
+        passwordInput.sendKeys("test12")
+        loginButton.click();
+
+        browser.sleep(5000)
+
+        expect(validationMessage.getText()).toContain('Login was unsuccessful. Please correct the errors and try again.\n' +
+            'The credentials provided are incorrect');
 
     })
 
