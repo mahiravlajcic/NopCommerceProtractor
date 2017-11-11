160a5d547e1abe23a6bf691ac7ac7baeb1aa9391
mahiravlajcic <33397801+mahiravlajcic@users.noreply.github.com>
2017-11-11 10:29:49 +0100

    serch-test.js

--- Search-test.js
+++ Search-test.js
@@ -23,4 +23,53 @@ describe ('Search page', function() {
 
     })
 
-})
+    it('Verify logo is present', function () {
+        var logo = $('[alt="Your store name"]')
+        var nokiaImage = $('[src="http://localhost:4582/content/images/thumbs/0000002_banner_2.jpg"]')
+        var facebookIconInFooter = $('.networks').$('.facebook')
+
+
+        expect(logo.isPresent()).toBe(true);
+        expect(nokiaImage.isPresent()).toBe(true);
+        expect(facebookIconInFooter.isPresent()).toBe(true);
+
+
+
+    })
+
+    it('Verify logo is present', function () {
+        var twitterIconInFooter = $('[href="https://twitter.com/nopCommerce"]')
+        var rssIconInFooter = $('[href="/news/rss/1"]')
+        var youTubeIconInFooter = $('[href="http://www.youtube.com/user/nopCommerce"]')
+        var googlePlusIconInFooter = $('[href="https://plus.google.com/+nopcommerce"]')
+
+            browser.sleep(5000)
+
+
+        expect(twitterIconInFooter.isPresent()).toBe(true);
+        expect(rssIconInFooter.isPresent()).toBe(true);
+        expect(youTubeIconInFooter.isPresent()).toBe(true);
+        expect(googlePlusIconInFooter.isPresent()).toBe(true);
+
+
+
+    })
+
+    it('Verify logo is present', function () {
+        var facebookIconInFooter = $('.networks').$('.facebook')
+        var twitterIconInFooter = $('.networks').$('.twitter')
+        var rssIconInFooter = $('.networks').$('.rss')
+        var youTubeIconInFooter = $('.networks').$('.youtube')
+        var googlePlusIconInFooter = $('.networks').$('.google-plus')
+
+
+        expect(twitterIconInFooter.isPresent()).toBe(true);
+        expect(rssIconInFooter.isPresent()).toBe(true);
+        expect(youTubeIconInFooter.isPresent()).toBe(true);
+        expect(googlePlusIconInFooter.isPresent()).toBe(true);
+
+
+
+
+    })
+     })
